bc620d4d9db93e85e4fe1883b7bf1cf94f0acdb4
raeperd <raeperd117@gmail.com>
2023-07-30 21:16:13 +0900

    ci: Fix base dir for github page deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://raeperd.github.io",
+  base: "/resume",
   integrations: [tailwind()],
   vite: {
     ssr: {
